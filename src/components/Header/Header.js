43b186422b0ffcb35bd2e3ff0b19085dbe88190d
jhonrsalcedo <jhonfor@gmail.com>
2019-05-12 23:47:34 -0500

    creando Header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Link} from 'react-router-dom'
+
+//State functional components
+class Header extends Component {
+
+    render() { 
+        return ( 
+            <header className="col-12 col-md-8">
+                <Link to={'/'}>
+                    <h1 className="text-center">React Crud</h1>
+                </Link>
+
+            </header>
+         );
+    }
+}
+ 
+export default Header;
---
+++ src/components/Router.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import axios from 'axios';
+import Header from '../components/Header/Header';
+
+class Router extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (  
+            <Header />
+        );
+    }
+}
+ 
+export default Router;
